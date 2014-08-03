c85ce776457d417e75c05e0bd4dbf81a969fab32
Gora Mohanty <gora@mimirtech.com>
2014-08-03 22:13:15 +0530

    Added depends for opsworks_java instead of java

--- hipsnip-jetty/metadata.rb
+++ hipsnip-jetty/metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.9.0"
 supports 'ubuntu', ">= 12.04"
 
-depends "java", ">= 1.10.0"
+depends "opsworks_java", ">= 1.0.0"
